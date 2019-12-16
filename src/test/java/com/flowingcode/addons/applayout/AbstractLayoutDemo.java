cc5af8305728d2653d8ffb90e1e62d165ba37a17
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2019-12-16 11:00:25 -0300

    Add AbstractFcAppRouterLayout demo

---
+++ src/test/java/com/flowingcode/addons/applayout/AbstractLayoutDemo.java
@@ -0,0 +1,21 @@
+package com.flowingcode.addons.applayout;
+
+import com.flowingcode.addons.applayout.menu.MenuItem;
+import com.vaadin.flow.component.HasElement;
+
+public class AbstractLayoutDemo extends AbstractFcAppRouterLayout{
+
+	@Override
+	protected void configure(AppLayout app) {
+		app.setMenuItems(
+			new MenuItem("Item 1"),
+			new MenuItem("Item 2")
+		);		
+	}
+
+	@Override
+	public void showRouterLayoutContent(AppLayout app, HasElement content) {
+		super.showRouterLayoutContent(app, content);
+	}
+	
+}
