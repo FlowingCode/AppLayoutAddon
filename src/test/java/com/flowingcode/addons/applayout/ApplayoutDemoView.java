0ed454bda66ef360ed375431a0a94a9d3abc3f59
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-07-12 16:16:20 -0300

    chore: update copyright header

--- src/test/java/com/flowingcode/addons/applayout/ApplayoutDemoView.java
+++ src/test/java/com/flowingcode/addons/applayout/ApplayoutDemoView.java
@@ -2,7 +2,7 @@
  * #%L
  * App Layout Addon
  * %%
- * Copyright (C) 2018 - 2020 Flowing Code
+ * Copyright (C) 2018 - 2021 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
