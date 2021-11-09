2b9f86c643f5c25f875de24c06edb2dcf09a8aff
Martín López <mlopez@flowingcode.com>
2021-11-09 13:49:14 -0300

    build: use fc-menuitem version 0.9.9

--- src/main/java/com/flowingcode/addons/applayout/MenuItem.java
+++ src/main/java/com/flowingcode/addons/applayout/MenuItem.java
@@ -39,7 +39,7 @@ import java.util.function.Consumer;
 @NpmPackage(value = "@polymer/paper-item", version = "3.0.1")
 @Tag("fc-menuitem")
 @JsModule("@flowingcode/fc-menuitem/fc-menuitem.js")
-@NpmPackage(value = "@flowingcode/fc-menuitem", version = "~0.9.8")
+@NpmPackage(value = "@flowingcode/fc-menuitem", version = "~0.9.9")
 public class MenuItem extends SlottedMenuItem
     implements HasOrderedComponents, HasMenuItemCommands<MenuItem>, HasMenuItemIcon<MenuItem> {
 
