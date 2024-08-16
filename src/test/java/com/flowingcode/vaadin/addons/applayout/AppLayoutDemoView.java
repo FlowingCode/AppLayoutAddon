8aae9c095265481249842f545c7cba127c81c97f
Martin Lopez <mlopez@flowingcode.com>
2024-08-16 18:22:10 -0300

    refactor: rename ApplayoutDemoView to AppLayoutDemoView

--- src/test/java/com/flowingcode/vaadin/addons/applayout/AppLayoutDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/applayout/AppLayoutDemoView.java
@@ -1,15 +1,15 @@
 /*-
  * #%L
- * App Layout Addon
+ * Template Add-on
  * %%
- * Copyright (C) 2018 - 2022 Flowing Code
+ * Copyright (C) 2023 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,22 @@
  */
 package com.flowingcode.vaadin.addons.applayout;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
+import com.flowingcode.vaadin.addons.DemoLayout;
+import com.flowingcode.vaadin.addons.GithubLink;
+import com.flowingcode.vaadin.addons.demo.TabbedDemo;
+import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
-@Route("")
-public class DemoView extends VerticalLayout implements BeforeEnterObserver {
+@ParentLayout(DemoLayout.class)
+@Route("applayout")
+@GithubLink("https://github.com/FlowingCode/ExtendedLoginAddon")
+public class AppLayoutDemoView extends TabbedDemo {
 
-  @Override
-  public void beforeEnter(BeforeEnterEvent event) {
-    event.forwardTo(AppLayoutDemoView.class);
+  public AppLayoutDemoView() {
+    addDemo(AppLayoutDemo.class);
+    addDemo(ExtendingAppLayoutDemo.class);
+    
+    setSizeFull();
   }
 }
